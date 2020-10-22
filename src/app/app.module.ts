5048cb08c31c4c24ab574be002f38c214bb41e5f
veeko18 <45859068+veeko18@users.noreply.github.com>
2020-10-22 23:23:52 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
+/* import { AutofocusModule } from 'angular-autofocus-fix'; */
+/* import { BrowserAnimationsModule } from '@angular/platform-browser/animations';*/
+
+@NgModule({
+  declarations: [AppComponent, TodoListComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule /* AutofocusModule */,
+    /* BrowserAnimationsModule */
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
